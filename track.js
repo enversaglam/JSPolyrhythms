022cd836ae72c683786e86d70a8ee80c86e6da4c
enversaglam <enversaglam@gmail.com>
2025-01-22 00:13:39 +0100

    track last

--- track.js
+++ track.js
@@ -6,14 +6,23 @@ class Track {
 
   getPosition(offset) {
     return {
-      x: this.center.x + this.radius * Math.cos(offset),
+      x: this.center.x + this.radius * Math.cos(offset * 3),
       y: this.center.y - this.radius * Math.sin(offset)
     };
   }
 
   draw(ctx) {
     ctx.beginPath();
-    ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
+    //before version
+    /* ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI); */
+    for (let a=0; a < 2 * Math.PI; a += 0.1) {
+      ctx.lineTo(
+        this.center.x + this.radius * Math.cos(a * 3),
+        this.center.y - this.radius * Math.sin(a)
+      );
+    }
+    ctx.closePath();
+    
     ctx.strokeStyle = 'white';
     ctx.stroke();
   }
