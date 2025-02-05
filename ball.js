1e12f0073afdacfdaf186b48f40a255043f51a61
enversaglam <enversaglam@gmail.com>
2025-02-05 01:38:30 +0100

    Enhance ball and track classes with hue support and progress tracking for improved visual effects

--- ball.js
+++ ball.js
@@ -1,11 +1,13 @@
 class Ball {
-    constructor(track, radius, speed, soundFrequency) {
+    constructor(track, radius, speed, soundFrequency, hue) {
         this.track = track;
         this.radius = radius;
         this.speed = speed;
         this.soundFrequency = soundFrequency;
+        this.hue = hue;
         this.offset = 0;
         this.round = 0;
+        this.progress = 0;
         this.center = this.track.getPosition(this.offset);
     }
     
@@ -13,6 +15,7 @@ class Ball {
         this.offset += this.speed;
         const res = this.track.getPosition(this.offset);
         this.center = { x: res.x, y: res.y };
+        this.progress = res.progress;
         if (res.round != this.round) {
             playSound(this.soundFrequency);
             this.round = res.round;
@@ -20,18 +23,14 @@ class Ball {
     }
 
     draw(ctx) {
-        const fakeY = 2 * this.track.center.y - this.center.y;
-        if (fakeY > this.center.y) {
-            ctx.beginPath();
-            ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
-            ctx.strokeStyle = 'white';
-            ctx.stroke();
-        } else {
-            ctx.beginPath();
-            ctx.arc(this.center.x, fakeY, this.radius, 0, 2 * Math.PI);
-            ctx.strokeStyle = 'white';
-            ctx.stroke();
-        }
+        ctx.beginPath();
+        ctx.arc(this.center.x, this.center.y, this.radius, 0, 2 * Math.PI);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = 'white';
+        const lightness = 100 - 50 * this.progress;
+        ctx.fillStyle = `hsl(${this.hue}, 100%, ${lightness}%)`;
+        ctx.fill();
+        ctx.stroke();
     }
 
 }
--- track.js
+++ track.js
@@ -1,15 +1,17 @@
 class Track {
-  constructor(center, radius) {
+  constructor(center, radius, hue) {
     this.center = center;
     this.radius = radius;
+    this.hue = hue;
     this.period = Math.PI;
   }
 
   getPosition(offset) {
     return {
       x: this.center.x + this.radius * Math.cos(offset),
-      y: this.center.y - this.radius * Math.sin(offset),
-      round: Math.floor(offset / this.period)
+      y: this.center.y - this.radius * Math.abs(Math.sin(offset)),
+      round: Math.floor(offset / this.period),
+      progress: (offset % this.period) / this.period
     };
   }
 
@@ -22,8 +24,7 @@ class Track {
       ctx.lineTo(pos.x, pos.y);
     }
     ctx.closePath();
-    
-    ctx.strokeStyle = 'white';
+    ctx.strokeStyle = `hsl(${this.hue}, 100%, 50%)`;
     ctx.stroke();
   }
 }
